c0aeaac44cea2a335381b671ed709283c7c4ea62
Francesco Baccaro <baccaro.f@gmail.com>
2016-09-07 14:37:11 +0000

    test file hook and execute.php

--- hook.php
+++ hook.php
@@ -1,25 +1,25 @@
 <?php
-// Load composer
-require __DIR__ . '/vendor/autoload.php';
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
 
-$commands_path = __DIR__ . '/Commands/';
-
-
-$API_KEY = '297809022:AAHaM0c6-mE2PvrFlEnV7JeHnKXor7JCSgM';
-$BOT_NAME = 'MovieRecSysBot';
-try {
-    // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
-	$telegram->addCommandsPath($commands_path);
-    // Handle telegram webhook request
-	$telegram->handle();
-	} catch (Longman\TelegramBot\Exception\TelegramException $e) {
-	    // Silence is golden!
-	    echo $e;
-	    // Log telegram errors
-	    //Longman\TelegramBot\TelegramLog::error($e);
-	} catch (Longman\TelegramBot\Exception\TelegramLogException $e) {
-	    // Silence is golden!
-	    // Uncomment this to catch log initilization errors
-	    echo $e;
+if(!$update)
+{
+  exit;
 }
+
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+
+$text = trim($text);
+$text = strtolower($text);
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
