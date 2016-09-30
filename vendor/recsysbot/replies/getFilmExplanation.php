291d5a8bfe819caab33a6d687aefe0d0b7691720
Francesco Baccaro <baccaro.f@gmail.com>
2016-09-30 18:18:02 +0000

    test image 8

--- vendor/recsysbot/replies/getFilmExplanation.php
+++ vendor/recsysbot/replies/getFilmExplanation.php
@@ -1,9 +1,10 @@
 <?php 
  
 use GuzzleHttp\Client;
-use Telegram\Bot\FileUpload\InputFile; 
+use Telegram\Bot\FileUpload\InputFile;
 
 function getFilmExplanation($telegram, $chatId, $movie){
+
    $movieName = str_replace(' ', '_', $movie); 
    $userID = 8;
    $client = new Client(['base_uri'=>'http://193.204.187.192:8080']);
@@ -26,56 +27,56 @@ function getFilmExplanation($telegram, $chatId, $movie){
          $property = str_replace("http://dbpedia.org/resource/", "", $v[2]);
          $property = str_replace('_', ' ', $property); // Replaces all underscore with spaces.
 
-            switch ($propertyType) {
-               case "/directors": case "directors": case "director":
-                  $directors[] = $property;
-                  break;
-               case "/starring": case "starring":
-                  $starring[] = $property;
-                  break;
-               case "/categories": case "categories": case "category": case "http://purl.org/dc/terms/subject":
-                  $property = str_replace("Category:", "", $property);
-                  $categories[] = $property;
-                  break;
-               case "/genres": case "genres": case "genre":
-                   $genres[] = $property;
-                  break;
-               case "/runtime": case "runtime":       
-                  $runtime = $property;
-                  break;
-               case "/writers": case "writers": case "writer":
-                   $writers[] = $property;
-                  break;
-               case "/producers": case "producers": case "producer":
-                   $producers[] = $property;
-                  break;
-               case "/release date": case "release date": case "releaseDate":
-                  $releaseDate = $property;
-                  break;
-               case "/music composers": case "music composers": case "music composer": case "musicComposer":
-                  //no
-                  $musicComposers[] = $property;
-                  break;
-               case "/cinematographies": case "cinematographies": case "cinematography":
-                   $cinematographies[] = $property;
-                  break;
-               case "/based on": case "based on": case "basedOn":
-                  $based[] = $property;
-                  break;
-               case "/editings": case "editings": case "editing":
-                  $editings[] = $property;
-                  break;
-               case "/distributors": case "distributors": case "distributor":
-                  $distributors[] = $property;
-                  break;
-               case "poster":
-                  $poster = $property;
-                  break;
-               case "title":
-                  $title = $property;
-                  break;
-               default:
-                  break;
+         switch ($propertyType) {
+            case "/directors": case "directors": case "director":
+               $directors[] = $property;
+               break;
+            case "/starring": case "starring":
+               $starring[] = $property;
+               break;
+            case "/categories": case "categories": case "category": case "http://purl.org/dc/terms/subject":
+               $property = str_replace("Category:", "", $property);
+               $categories[] = $property;
+               break;
+            case "/genres": case "genres": case "genre":
+                $genres[] = $property;
+               break;
+            case "/runtime": case "runtime":       
+               $runtime = $property;
+               break;
+            case "/writers": case "writers": case "writer":
+                $writers[] = $property;
+               break;
+            case "/producers": case "producers": case "producer":
+                $producers[] = $property;
+               break;
+            case "/release date": case "release date": case "releaseDate":
+               $releaseDate = $property;
+               break;
+            case "/music composers": case "music composers": case "music composer": case "musicComposer":
+               //no
+               $musicComposers[] = $property;
+               break;
+            case "/cinematographies": case "cinematographies": case "cinematography":
+                $cinematographies[] = $property;
+               break;
+            case "/based on": case "based on": case "basedOn":
+               $based[] = $property;
+               break;
+            case "/editings": case "editings": case "editing":
+               $editings[] = $property;
+               break;
+            case "/distributors": case "distributors": case "distributor":
+               $distributors[] = $property;
+               break;
+            case "poster":
+               $poster = $property;
+               break;
+            case "title":
+               $title = $property;
+               break;
+            default:
+               break;
          }
       }
    }
@@ -142,4 +143,7 @@ function getFilmExplanation($telegram, $chatId, $movie){
    if ($releaseDate !== '') {$telegram->sendChatAction(['chat_id' => $chatId, 'action' => 'typing']);
    $telegram->sendMessage(['chat_id' => $chatId, 'text' => "Release dates: ".$releaseDate]);}
 
+
+
 }
+   
