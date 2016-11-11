478b0e9fd8a8a2270b18dc1ca6ef5e04403fe498
Francesco Baccaro <baccaro.f@gmail.com>
2016-11-11 15:00:56 +0100

    config movie

--- vendor/recsysbot/config/movierecsysbot-config.php
+++ vendor/recsysbot/config/movierecsysbot-config.php
@@ -3,8 +3,8 @@
  * @author Francesco Baccaro
  */
 return [
-    // Telegram TOKEN testrecsysbot
-    'token' => '187641008:AAFqlSfKjk3XK3onRNpt2qJczjhpCg0_-HA',
+    // Telegram TOKEN movierecsysbot
+    'token' => '297809022:AAHaM0c6-mE2PvrFlEnV7JeHnKXor7JCSgM',
     //The timezone setting, Guzzle suggests having this for proper requests/responses
     'timezone' => 'Europe/Rome',
     //If no response is found, this will be used as response
