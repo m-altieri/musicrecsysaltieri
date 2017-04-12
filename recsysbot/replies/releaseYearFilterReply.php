c57d4653b03531e4791f1d1daf7bcb72fffda9ca
Francesco Baccaro <baccaro.f@gmail.com>
2017-04-12 21:36:27 +0200

    creato il fullbot 6

--- recsysbot/replies/releaseYearFilterReply.php
+++ recsysbot/replies/releaseYearFilterReply.php
@@ -27,33 +27,51 @@ function releaseYearFilterReply($telegram, $chatId, $propertyType, $propertyValu
    $numberRatedProperties = getNumberRatedProperties($chatId);
    $needNumberOfRatedProperties = 3 - ($numberRatedProperties + $numberRatedMovies);
 
-   if ($needNumberOfRatedProperties <= 0) {   
-      $text = "Do you prefer to tell me something else about you \nor can I recommend you a movie?";
+   $pagerankCicle = getNumberPagerankCicle($chatId);
 
-      $pagerankCicle = getNumberPagerankCicle($chatId);
-      $movie =  oldRecMovieToRefineSelected($chatId, $pagerankCicle);
-      file_put_contents("php://stderr", "Rate other properties of: ".$movie.PHP_EOL);    
-      if ($movie !== "null") {
+   if ($needNumberOfRatedProperties <= 0) {
+
+      $movie = recMovieToRefineSelected($chatId, $pagerankCicle);
+
+      file_put_contents("php://stderr", "conf4userPropertyValueRatingReply - movie:".$movie." - pagerankCicle:".$pagerankCicle.PHP_EOL);  
+      
+      if (strcasecmp($movie, "null") !== 0 && $pagerankCicle >= 0) {
+         $text = "Do you prefer rate other properties of "."\"".ucwords($movie)."\" \nor Back to movies?";
          $keyboard = [
                         ["🔎 Rate other properties of "."\"".ucwords($movie)."\""],
                         ["🔙 Back to Movies"]
                     ];
+         $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+
+         $telegram->sendChatAction(['chat_id' => $chatId, 'action' => 'typing']);
+         $telegram->sendMessage(['chat_id' => $chatId, 'text' => $text, 'reply_markup' => $reply_markup]);
       } 
       else {
+         //$text = "Do you want tell me something else about you?";
+         $text = "Let me recommend a movie 😃";
+         $text .= "\nTap on \"🌐 Recommend Movies\" button, otherwise you can enrich your profile by providing further ratings 😉";
+         //$text = "\nLet me recommend a movie 😃\n(tap \"🌐 Recommend Movies\")\n\nOr type your preference\n(e.g., Pulp Fiction or Tom Cruise or Thriller) 🙂";
          $keyboard = userPropertyValueKeyboard();
+         
+         $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+         $telegram->sendChatAction(['chat_id' => $chatId, 'action' => 'typing']);
+         $telegram->sendMessage(['chat_id' => $chatId, 'text' => $text, 'reply_markup' => $reply_markup]);
       }
-      
-      
+
    }
+   //new user
    else{
       $text = "Do you want tell me something else about you?";
+      //$text = "Do you want tell me something else about you?\n\nPlease, type your preference\n(e.g., Pulp Fiction or Tom Cruise or Thriller) 🙂";
       $keyboard = startProfileAcquisitionKeyboard();
+      
+      $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
+      $telegram->sendChatAction(['chat_id' => $chatId, 'action' => 'typing']);
+      $telegram->sendMessage(['chat_id' => $chatId, 'text' => $text, 'reply_markup' => $reply_markup]);
+
+ 
    }
 
-   $reply_markup = $telegram->replyKeyboardMarkup(['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]);
-
-   $telegram->sendChatAction(['chat_id' => $chatId, 'action' => 'typing']);
-   $telegram->sendMessage(['chat_id' => $chatId, 'text' => $text, 'reply_markup' => $reply_markup]);
-
 }
-
+//$text = "Do you prefer to tell me something else about you \nor can I recommend you a movie?";
+//$text = "Do you want tell me something else about you?";
