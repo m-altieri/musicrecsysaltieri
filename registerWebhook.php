c1df22250698bf3476b3850f1769df727a4a4530
m-altieri <m.altieri13@studenti.uniba.it>
2017-10-26 21:47:26 +0200

    register 2

--- registerWebhook.php
+++ registerWebhook.php
@@ -21,7 +21,7 @@ $response = [
 		'message' => [ 'text' => $answer ]
 ];
 $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_GET, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_exec($ch);
