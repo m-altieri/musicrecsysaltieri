6ea99a48bfc3dbfe2ab40ab72fb7050356de40ea
m-altieri <m.altieri13@studenti.uniba.it>
2017-10-30 22:48:22 +0100

    put user detail 3.1

--- recsysbot/restService/putUserDetail.php
+++ recsysbot/restService/putUserDetail.php
@@ -4,7 +4,7 @@ function putUserDetail($chatId, $firstname, $lastname, $username) {
 	$userID = $chatId;
 	// $client = new Client(['base_uri'=>'http://localhost:8080']);
 	$client = new Client ( [ 
-			'base_uri' => 'http://127.0.0.1:8080' 
+			'base_uri' => 'http://localhost:8080' 
 	] );
 	$stringGetRequest = '/movierecsysrestful/restService/detail/putUserDetail?userID=' . $userID . '&firstname=' . $firstname . '&lastname=' . $lastname . '&username=' . $username;
 	$response = $client->request ( 'GET', $stringGetRequest );
