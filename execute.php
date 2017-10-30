89066ee0312288105f3497f7a2b1ae318457a3a2
m-altieri <m.altieri13@studenti.uniba.it>
2017-10-30 17:12:13 +0100

    send 1.1

--- execute.php
+++ execute.php
@@ -253,7 +253,7 @@
 		];
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($res))
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($res));
 		curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 		curl_exec($ch);
 		curl_close($ch);
