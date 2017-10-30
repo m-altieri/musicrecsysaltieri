59da076d15eef63a7351df1b862f1f681f1cf4ac
m-altieri <m.altieri13@studenti.uniba.it>
2017-10-30 23:16:33 +0100

    put user detail 3.4 (senza usare guzzle)

--- recsysbot/restService/putUserDetail.php
+++ recsysbot/restService/putUserDetail.php
@@ -3,14 +3,19 @@ use GuzzleHttp\Client;
 function putUserDetail($chatId, $firstname, $lastname, $username) {
 	$userID = $chatId;
 	// $client = new Client(['base_uri'=>'http://192.168.1.107:8080']);
-	$client = new Client ( [ 
-			'base_uri' => 'http://193.204.187.192:8080' 
-	] );
+// 	$client = new Client ( [ 
+// 			'base_uri' => 'http://193.204.187.192:8080' 
+// 	] );
 	$stringGetRequest = '/movierecsysrestful/restService/detail/putUserDetail?userID=' . $userID . '&firstname=' . $firstname . '&lastname=' . $lastname . '&username=' . $username;
-	$response = $client->request ( 'GET', $stringGetRequest );
-	$bodyMsg = $response->getBody ()->getContents ();
-	$data = json_decode ( $bodyMsg );
+// 	$response = $client->request ( 'GET', $stringGetRequest );
+// 	$bodyMsg = $response->getBody ()->getContents ();
+// 	$data = json_decode ( $bodyMsg );
+	$ch = curl_init("http://193.204.187.192:8080" . $stringGetRequest);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$res = curl_exec($ch);
+	curl_close($ch);
 	//192.168.1.107
+	file_put_contents("php://stderr", $res . PHP_EOL);
 	file_put_contents ( "php://stderr", "http://193.204.187.192:8080" . $stringGetRequest . "/return:" . $data . PHP_EOL );
 	
 	return $data;
