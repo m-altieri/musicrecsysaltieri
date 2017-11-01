ebf9d97163057b1efb02576ddb551a0dbc71c551
m-altieri <m.altieri13@studenti.uniba.it>
2017-11-01 09:02:02 +0100

    persistent menu 2.3.3

--- recsysbot/facebook/setPersistentMenu.php
+++ recsysbot/facebook/setPersistentMenu.php
@@ -38,7 +38,7 @@ function setPersistentMenu() {
 	
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req));
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER,  ['Content-Type: application/json']);
 	$res = curl_exec($ch);
