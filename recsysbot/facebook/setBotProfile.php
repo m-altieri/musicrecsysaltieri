6d80d32f0944255a61754bf87db652f8c8ade987
m-altieri <m.altieri13@studenti.uniba.it>
2017-11-02 14:01:20 +0100

    emoji 1.4

--- recsysbot/facebook/setBotProfile.php
+++ recsysbot/facebook/setBotProfile.php
@@ -2,6 +2,8 @@
 
 function setBotProfile() {
 	
+	$config = require '/app/recsysbot/config/movierecsysbot-config.php';
+	
 	setGreeting($config['greeting']);
 	setGetStarted("get_started");
 	setPersistentMenu();
