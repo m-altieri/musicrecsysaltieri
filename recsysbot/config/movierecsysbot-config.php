f5e256d1654690a954ab9700ecfd33a5b124ffb8
negro <negro@recsyserver>
2017-11-25 18:17:16 +0100

    collegamento 5.1

--- recsysbot/config/movierecsysbot-config.php
+++ recsysbot/config/movierecsysbot-config.php
@@ -7,9 +7,9 @@ $emojis = require '/app/recsysbot/variables/emojis.php';
 
 return [ 
 		// Facebook Token Test movierecsys
-		'token' => 'EAAE0Lnad6ywBAGrnw7jHREnIc0CiZAuOpLV8iykP1WOFS8ykXFeoLm4340Js0ZCmZCdZAH6wwpkV6Lr5PoeWZA7b8miJP9vUjfWerd8rF9v95ORNWhdMPUFJmjZBhl4nxW0DDaCImlZBjgqYFpewFhHtVAiHkMBbgKgVVwT7E2lswZDZD',
+// 		'token' => 'EAAE0Lnad6ywBAGrnw7jHREnIc0CiZAuOpLV8iykP1WOFS8ykXFeoLm4340Js0ZCmZCdZAH6wwpkV6Lr5PoeWZA7b8miJP9vUjfWerd8rF9v95ORNWhdMPUFJmjZBhl4nxW0DDaCImlZBjgqYFpewFhHtVAiHkMBbgKgVVwT7E2lswZDZD',
 		// Telegram TOKEN movierecsysbot
-// 		'token' => '422658992:AAH1c7kkVvuAIIuVDbbek7Mo4Zd0pKSU8nM',
+		'token' => '422658992:AAH1c7kkVvuAIIuVDbbek7Mo4Zd0pKSU8nM',
 		// The timezone setting, Guzzle suggests having this for proper requests/responses
 		'timezone' => 'Europe/Rome',
 		// If no response is found, this will be used as response
