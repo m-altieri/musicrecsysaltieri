0505de91c4567b09ad99b529944fe31e8e4dd856
m-altieri <m.altieri13@studenti.uniba.it>
2017-11-27 20:33:41 +0100

    generalizzazione server 4

--- recsysbot/restService/putChatMessage.php
+++ recsysbot/restService/putChatMessage.php
@@ -1,15 +1,15 @@
 // <?php
 use GuzzleHttp\Client;
-$config = require_once '/app/recsysbot/config/movierecsysbot-config.php';
+$config = require '/app/recsysbot/config/movierecsysbot-config.php';
 
 function putChatMessage($chatId, $messageId, $context, $replyText, $replyFunctionCall, $pagerankCicle, $botName, $botTimestamp, $responseType) {
 	$userID = $chatId;
-	// $client = new Client(['base_uri'=>'http://localhost:8080']);
+	$base_uri = $config['base_uri'];
 	$client = new Client ( [ 
-			'base_uri' => $config['base_uri'] 
+			'base_uri' => $base_uri
 	] );
 	$stringGetRequest = $config['application_uri'] . '/restService/chatMessage/putChatMessage?userID=' . $userID . '&messageID=' . $messageId . '&context=' . $context . '&replyFunctionCall=' . $replyFunctionCall . '&replyText=' . urlencode ( $replyText ) . '&pagerankCicle=' . $pagerankCicle . '&botName=' . $botName . '&botTimestamp=' . $botTimestamp . '&responseType=' . $responseType;
-	file_put_contents ( "php://stderr", $config['base_uri'] . $stringGetRequest . "/return:" . $data . PHP_EOL );
+	file_put_contents ( "php://stderr", $base_uri . $stringGetRequest . "/return:" . $data . PHP_EOL );
 	
 	$response = $client->request ( 'GET', $stringGetRequest );
 	$bodyMsg = $response->getBody ()->getContents ();
