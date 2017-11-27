698a1117644b1d953e0ecece278e189727578a87
m-altieri <m.altieri13@studenti.uniba.it>
2017-11-27 18:05:35 +0100

    debugging 3

--- recsysbot/keyboards/resetProfileKeyboard.php
+++ recsysbot/keyboards/resetProfileKeyboard.php
@@ -1,9 +1,12 @@
 <?php
 function resetProfileKeyboard() {
+	
+	$emojis = require '/app/recsysbot/variables/emojis.php';
+	
 	$keyboard = [ 
 			[ 
-					$emojis['blacksquarebutton'].'All Properties',
-					$emojis['whitesquarebutton'].'All Movies' 
+					$emojis['blacksquarebutton'].' All Properties',
+					$emojis['whitesquarebutton'].' All Movies' 
 			],
 			[ 
 					"".$emojis['wastebasket']." Delete all preferences" 
