6b41d88be91dd8b2fcb15eb3c2b579f68acba7a1
m-altieri <m.altieri13@studenti.uniba.it>
2017-11-28 20:58:23 +0100

    stateless 3

--- recsysbot/config/movierecsysbot-config.php
+++ recsysbot/config/movierecsysbot-config.php
@@ -21,8 +21,8 @@ return [
 		// Facebook payload returned from "Start" button
 		'getStartedPayload' => "get_started",
         // Base URI (Altieri)
-        'base_uri' => '193.204.187.192:8080', //DA MODIFICARE
+        'base_uri' => '193.204.187.192:8090', //DA MODIFICARE
 		// Server application URI (Altieri)
-		'application_uri' => '/movierecsysrestful' //DA MODIFICARE
+		'application_uri' => '/movierecsysservice' //DA MODIFICARE
 ];
 ?>
