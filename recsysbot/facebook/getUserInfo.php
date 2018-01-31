00fb0f26d92d847457bd396d957196e96ee12e2b
m-altieri <m.altieri13@studenti.uniba.it>
2018-01-31 12:18:05 +0100

    fix

--- recsysbot/facebook/getUserInfo.php
+++ recsysbot/facebook/getUserInfo.php
@@ -5,7 +5,7 @@ require '/app/recsysbot/facebook/utils.php';
 function getUserInfo($chat_id) {
 	
 	$url = 'https://graph.facebook.com/v2.6/' . $chat_id . '?fields=first_name,last_name&access_token=' . token();
-	$result = json_decode(file_get_contents($url), true);
+	$result = json_decode(file_get_contents($url, true));
 	file_put_contents("php://stderr", "\nUser info: " . $result);
 	return $result;
 }
