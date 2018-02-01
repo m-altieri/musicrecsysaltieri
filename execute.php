eacf58511ad0c0a2990e772adaf2c054e40490f8
m-altieri <m.altieri13@studenti.uniba.it>
2018-02-01 18:11:27 +0100

    fix

--- execute.php
+++ execute.php
@@ -25,6 +25,15 @@
 		require_once $file;
 	}
 	
+	ignore_user_abort(true);
+	set_time_limit(0);
+	ob_start();
+	header('Connection: close');
+	header('Content-Length: ' . ob_get_length());
+	ob_end_flush();
+	ob_flush();
+	flush();
+	
 	/**
 	 * Change platform here.
 	 * To create a new platform, create its own php class in the platforms folder.
@@ -36,7 +45,7 @@
 	$update = json_decode($content, true);
 	
 	if (!$update) {
-		exit ();
+		exit();
 	}
 
 	$messageInfo = $platform->getMessageInfo($update);
