2509e54b5fefa36534423a36163b9e7a279b2b74
m-altieri <m.altieri13@studenti.uniba.it>
2018-02-01 16:15:31 +0100

    fix problema con l'unix time

--- execute.php
+++ execute.php
@@ -49,6 +49,9 @@
 	}
 	
 	$messageInfo['text'] = trim ($messageInfo['text']);
+	if (strlen($messageInfo['date']) > 10) { // Se l'UNIX time è in millisecondi, lo converto in secondi
+		$messageInfo['date'] = substr($messageInfo['date'], 0, $messageInfo['date'] - 3);
+	}
 
 	// Metterlo nel config
 	$botName = "movierecsysbot";
