71816f49c3fdb4bdc058c235fd2cf1270c3c922a
m-altieri <m.altieri13@studenti.uniba.it>
2018-03-09 11:20:59 +0100

    auxAPI

---
+++ recsysbot/restService/getAuxReply.php
@@ -0,0 +1,20 @@
+<?php
+use GuzzleHttp\Client;
+
+function getAuxReply($url) {
+	
+	$baseUrl = explode('?', $url)[0];
+	$stringGetRequest = '?' . explode('?', $url)[1];
+	
+	$client = new Client ( [
+			'base_uri' => $baseUrl
+	] );
+	
+	file_put_contents ( "php://stderr", '[sendMessageToServer]' . $baseUrl . $stringGetRequest . PHP_EOL );
+	
+	$response = $client->request ( 'GET', $stringGetRequest );
+	$bodyMsg = $response->getBody ()->getContents ();
+	$data = json_decode ( $bodyMsg, true );
+	
+	return $data;
+}
