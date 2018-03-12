0bd25039911a904503b9ab5916e24e5ce7ba6980
m-altieri <m.altieri13@studenti.uniba.it>
2018-03-12 23:38:38 +0100

    Test

--- recsysbot/restService/getAuxReply.php
+++ recsysbot/restService/getAuxReply.php
@@ -2,43 +2,36 @@
 use GuzzleHttp\Client;
 
 function getAuxReply($auxAPI) {
+	file_put_contents("php://stderr", print_r("auxAPI: \n" . $auxAPI, true) . PHP_EOL);
 	
 	$apiURL = $auxAPI['apiURL'];
 	
 	$baseUrl = explode('?', $apiURL)[0];
 	$stringGetRequest = '?' . explode('?', $apiURL)[1];
 	
+	$client = new Client ([
+			'base_uri' => $baseUrl
+	]);
+	
+	$response = null;
+	
 	if ($auxAPI['parameters'] == null) { // E' una richiesta GET
-		
-		$client = new Client ([
-				'base_uri' => $baseUrl
-		]);
-		
+
 		file_put_contents("php://stderr", '[auxAPI GET] ' . $baseUrl . $stringGetRequest . PHP_EOL);
 		
 		$response = $client->request('GET', $stringGetRequest);
-		$bodyMsg = $response->getBody()->getContents();
-		$data = json_decode($bodyMsg, true);
 		
 	} else { // E' una richiesta POST
 		
 		file_put_contents("php://stderr", '[auxAPI POST] ' . $baseUrl . $stringGetRequest . PHP_EOL);
-		file_put_contents("php://stderr", print_r("auxAPI: \n" . $auxAPI, true) . PHP_EOL);
-		file_put_contents("php://stderr", print_r("auxAPI parameters: \n" . $auxAPI['parameters'], true) . PHP_EOL);
-		
-		$client = new Client ([
-				'base_uri' => $baseUrl
-		]);
 		
 		$response = $client->request('POST', $stringGetRequest, [
 				'json' => $auxAPI['parameters']
 		]);
-		
-		$bodyMsg = $response->getBody()->getContents();
-		$data = json_decode($bodyMsg, true);
-		// TODO
 	}
 	
+	$bodyMsg = $response->getBody()->getContents();
+	$data = json_decode($bodyMsg, true);
 	
 	return $data;
 }
