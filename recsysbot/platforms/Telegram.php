2ffc4fa12687f28fda95c482a7a9923b7047da2f
m-altieri <m.altieri13@studenti.uniba.it>
2018-03-14 00:47:37 +0100

    test

--- recsysbot/platforms/Telegram.php
+++ recsysbot/platforms/Telegram.php
@@ -75,7 +75,7 @@ class Telegram implements Platform {
 	
 	public function sendLink($chat_id, $text, $url, $reply_markup) {
 
-		$request_url = "https://api.telegram.org/bot" . $config['telegram_token'] . "/sendMessage";
+		$request_url = "https://api.telegram.org/bot" . $this->config['telegram_token'] . "/sendMessage";
 		file_put_contents("php://stderr", "Text: " . $text . PHP_EOL);
 		$parameters = [
 				'chat_id' => $chat_id,
